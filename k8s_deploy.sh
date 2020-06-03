dbdd853ebcd3bec255bb88e758cfd951a1e9e6ba
Renato Dantas <renato.srdantas@gmail.com>
2020-06-03 01:23:22 -0300

    fix deploy sh

--- k8s_deploy.sh
+++ k8s_deploy.sh
@@ -2,9 +2,9 @@
 
 ./gradlew clean build -x test
 
-sudo docker build -t oauth .
-sudo docker tag oauth:latest 145156591738.dkr.ecr.sa-east-1.amazonaws.com/oauth:latest
-sudo docker push 145156591738.dkr.ecr.sa-east-1.amazonaws.com/oauth:latest
+docker build -t oauth .
+docker tag oauth:latest 145156591738.dkr.ecr.sa-east-1.amazonaws.com/oauth:latest
+docker push 145156591738.dkr.ecr.sa-east-1.amazonaws.com/oauth:latest
 
 kubectl apply -f deploy/mongodb/deployment.yaml
 kubectl apply -f deploy/mongodb/service.yaml
